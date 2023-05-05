2fb0e9eb6320f60e27639af3ebb456ed7673b152
s26992-pj <116387081+s26992-pj@users.noreply.github.com>
2023-05-05 21:17:10 +0200

    Create zad2.cpp

---
+++ s26992-SebastianHellak/zajecia4/zad2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int wartosc1, wartosc2, wynik;
+	std::cout << "Podaj wartosc 1"<< std::endl;
+	std::cin >> wartosc1;
+	std::cout << "Podaj wartosc2" << std::endl;
+	std::cin >> wartosc2;
+	wynik = wartosc1 + wartosc2;
+	
+	return wynik;
+}
