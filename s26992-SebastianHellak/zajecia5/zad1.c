be53bb5b7b1114aad7ea96f550c1911dcd199564
s26992-pj <116387081+s26992-pj@users.noreply.github.com>
2023-05-31 17:47:12 +0200

    Create zad1.c

---
+++ s26992-SebastianHellak/zajecia5/zad1.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+int main(){
+	int value;
+  
+	printf("PID: %d\t, getpid());
+	printf("PPID: %d\n", getppid());
+	scanf("%d", &value);
+
+	return 0;
+}
